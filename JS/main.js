8de9d30c6bada083092aeadba619bd2144b88116
izm0 <34348672+izm0@users.noreply.github.com>
2018-01-10 00:38:56 +0000

    Merch page Added 9/1/18

--- JS/main.js
+++ JS/main.js
@@ -3,6 +3,54 @@ $(function(){
 	var myNav = $(".navtop");
 	var NavHeight = myNav.position().top;
 	var collapsed = false;
+	var bStorage = false;
+	var pTotal = 0;
+	if (typeof(Storage) !== "undefined") { //checking for local storage
+		bStorage = true;
+		var pickercol = localStorage.getItem("bancolour");
+		$(".colchange").css("background-color",pickercol);
+	} else {
+		$(".colchange").css("background-color","#0a84b3");
+	}
+	$(".clicked").css("background-color","#000000");
+	
+	$(".buybutton").click(function cartAdd(){
+		var item = this.closest(".item");
+		var itemname = $(item).data("itemname");
+		//console.log(itemname);
+		var itemprice = $(item).data("price");
+		//console.log(itemprice);
+	
+		var cartList = $("#itemlist > table > tbody > tr .name").filter(function(index){
+			return $(this).text() == itemname
+		});
+		if (cartList.length >0){
+			
+			var qty = $(cartList).parent().find(".quantity").text();
+			qty = parseInt(qty)+ 1;
+			$(cartList).parent().find(".quantity").text(qty); //updating quantity
+			
+			var cuPrice = $(cartList).parent().find(".price").text();
+			cuPrice= parseInt(cuPrice)+itemprice;
+			$(cartList).parent().find(".price").text(cuPrice); //updating price
+		}
+		else{
+			$("#itemlist > table > tbody").append("<tr><td class='name'>"+itemname+"</td><td class='price'>"+itemprice+"</td><td class='quantity'>1</td></tr>");	
+		}
+		
+		var priceList = $("#itemlist > table > tbody > tr .price");
+		pTotal =0;
+		for(var i=0;i<priceList.length;i++){
+			//Yu mu gway gway vi de sao
+			pTotal += parseInt($(priceList[i]).text());
+		}
+		$("#total").text("Total cost:"+parseInt(pTotal));
+	
+		
+	});
+
+	
+	
 	$(window).scroll(function stick() {
 		if ($(this).scrollTop() > NavHeight){
 			console.log("Applying class");
@@ -13,13 +61,19 @@ $(function(){
 		}
 		
 	});
-	
+
 	$(".picker").click(function(){
 		console.log("getting to here");
-		var pickercol = $(this).css("background-color");
+		pickercol = $(this).css("background-color");
 		console.log(pickercol+" = colour selected");
 		$(".colchange").css("background-color",pickercol);
-		
+		if (bStorage) {
+			localStorage.setItem("bancolour",pickercol);
+		} 
+		else {
+			//if local storage is not supported
+		}
+		$(".clicked").css("background-color","#000000");
 	});
 	
 	$("#collarrow").click(function(){
